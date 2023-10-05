a79a2a5309b6983905714d3d5bd3563010f9f60b
Sanju <141401968+sanju9636@users.noreply.github.com>
2023-10-05 12:07:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/OVER_POWEREDD")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Fun_Here")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/lovezone143")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/NeeJjeeaspirants07")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/1e2638b439b4b7d129d0b.jpg"
+    "START_IMG_URL", "https://te.legra.ph/file/25efe6aa029c6baea73ea.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
@@ -113,4 +113,4 @@ if SUPPORT_CHAT:
     if not re.match("(?:http|https)://", SUPPORT_CHAT):
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
-        )
+)
