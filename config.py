eed327828aa341a9f12519fc810b90f6baf209d8
Sanju <141401968+sanju9636@users.noreply.github.com>
2024-03-06 09:07:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/sanju9636/LOFI-MUSIC-PRO",
+    "https://github.com/sanju9636/LOFI-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
